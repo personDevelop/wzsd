da9b96d609767623fef2ed4e1e047873c0d87fa6
luyonglie <luyonglie@163.com>
2015-06-21 12:01:59 +0800

    为
    额外为二位

---
+++ 发布/Content/ueditor/UEditorCustomer.js
@@ -0,0 +1,15 @@
+﻿function getContent(editid) {
+    //Html编码获取Html转义实体
+    $('<div/>').text(UE.getEditor(editid).getContent()).html();
+}
+
+function setContent(editid, content, isAppendTo) {
+    //Html解码获取Html实体
+    UE.getEditor(editid).setContent($('<div/>').html(content).text(), isAppendTo);
+}
+function getContentTxt(editid) {
+    UE.getEditor(editid).getContentTxt();
+}
+function htmlDecode(value) {
+    return;
+}
