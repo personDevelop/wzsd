b8cb4dabdf17765c77f1455b93ebb202f54161b6
skiphyuk <liweilong0921@126.com>
2015-07-09 22:25:07 +0800

    增加兑换规则设置

---
+++ src/EasyCms.Web/Areas/Admin/Controllers/RedeemRulesController.cs
@@ -0,0 +1,116 @@
+﻿using EasyCms.Business;
+using EasyCms.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sharp.Common;
+using System.Data;
+using EasyCms.Web.Common;
+
+
+namespace EasyCms.Web.Areas.Admin.Controllers
+{
+    public class RedeemRulesController : Controller
+    {
+        RedeemRulesBll bll = new RedeemRulesBll();
+        //
+        // GET: /Admin/RedeemRules/
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+         
+        public string GetList(int pagenum, int pagesize)
+        {
+            int recordCount = 0;
+            System.Data.DataTable dt = bll.GetList(pagenum + 1, pagesize, ref   recordCount);
+
+            string result = JsonWithDataTable.Serialize(dt);
+            result = "{\"total\":\"" + recordCount.ToString() + "\",\"data\":" + result + "}";
+            return result;
+
+        }
+        public string GetListForSelecte(int pagenum, int pagesize)
+        {
+            int recordCount = 0;
+            System.Data.DataTable dt = bll.GetList(pagenum, pagesize, ref   recordCount, true);
+            string result = JsonWithDataTable.Serialize(dt);
+            result = "{\"total\":\"" + recordCount.ToString() + "\",\"data\":" + result + "}";
+            return result;
+        }
+        public string CheckRepeat(string ID, string ParentID, string RecordStatus, string val, bool IsCode)
+        {
+            return bll.Exit(ID, ParentID, RecordStatus, val, IsCode).ToString().ToLower();
+
+        }
+        //
+        // POST: /Admin/RedeemRules/Create
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Save(FormCollection collection)
+        {
+            RedeemRules p = null; ;
+
+            try
+            {
+                if (collection["RecordStatus"] != "add")
+                {
+                    p = bll.GetEntity(collection["ID"]);
+                    p.BindForm<RedeemRules>(collection);
+                }
+                else
+                {
+                    // TODO: Add insert logic here
+                    p = collection.Bind<RedeemRules>();
+
+                }
+
+                if (p.RecordStatus == Sharp.Common.StatusType.add)
+                {
+                    if (string.IsNullOrWhiteSpace(p.ID))
+                    {
+                        p.ID = Guid.NewGuid().ToString();
+                    }
+                    //p.CreatedUserID = Session.GetUserID();
+                }
+                bll.Save(p);
+                TempData.Add("IsSuccess", "保存成功");
+                ModelState.Clear();
+
+
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("error", ex.Message);
+
+            }
+            return View("Edit", p);
+        }
+
+        //
+        // GET: /Admin/RedeemRules/Edit/5
+        public ActionResult Edit(string id)
+        {
+
+            RedeemRules p = null;
+            if (string.IsNullOrWhiteSpace(id))
+            {
+                p = new RedeemRules();
+            }
+            else
+                p = bll.GetEntity(id);
+            return View("Edit", p);
+        }
+
+        [HttpPost]
+        //
+        // GET: /Admin/RedeemRules/Delete/5
+        public string Delete(string id)
+        {
+            return bll.Delete(id);
+        }
+    }
+}
