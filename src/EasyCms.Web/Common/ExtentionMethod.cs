6aabed69516645139dbeac279992ebd1aed42b5a
luyonglie <luyonglie@163.com>
2015-07-15 23:22:07 +0800

    阿萨德
    啊得撒撒的

--- src/EasyCms.Web/Common/ExtentionMethod.cs
+++ src/EasyCms.Web/Common/ExtentionMethod.cs
@@ -1,15 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 
-namespace EasyCms.Common
+namespace EasyCms 
 {
     public static class ExtentionMethod
     {
         public static bool IsJsonRequest(this HttpRequestBase request)
         {
             return "json" == request["format"];
+        }
+
+        public static string GetQueryValue(this HttpRequestMessage request, string key)
+        {
+            string value = null;
+            IEnumerable<KeyValuePair<string, string>> querystring = request.GetQueryNameValuePairs();
+            foreach (var item in querystring)
+            {
+                if (item.Key.ToLower() == key.ToLower())
+                {
+                    value = item.Value;
+                    break;
+                }
+            } 
+            return value; 
         }
 
 
