26d487167031ce961637b407542dd3dad6af9eba
luyonglie <luyonglie@163.com>
2015-07-19 23:47:43 +0800

    23
    23

---
+++ src/EasyCms.Model/NewsComment.cs
@@ -0,0 +1,342 @@
+﻿using Sharp.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+namespace EasyCms.Model
+{
+    /// <summary>
+    /// 新闻评论
+    /// </summary>  
+    [JsonObject]
+    public partial class NewsComment : BaseEntity
+    {
+        public static Column _ = new Column("NewsComment");
+
+        public NewsComment()
+        {
+            this.TableName = "NewsComment";
+            OnCreate();
+        }
+
+
+        #region 私有变量
+
+        private string _ID;
+
+        private string _ContentId;
+
+        private string _Description;
+
+        private DateTime _CreatedDate;
+
+        private string _CreatedUserID;
+
+        private int _ReplyCount;
+
+        private string _ParentID;
+
+        private string _TypeID;
+
+        private int _State;
+
+        private bool _IsRead;
+
+        private string _CreatedNickName;
+
+        #endregion
+
+        #region 属性
+
+        /// <summary>
+        ///  ID,
+        /// </summary>
+
+        [PrimaryKey]
+        [DbProperty(MapingColumnName = "ID", DbTypeString = "char", ColumnIsNull = false, IsUnique = true, ColumnLength = 36, ColumnJingDu = 0, IsGenarator = true, StepSize = 1, ColumnDefaultValue = "")]
+
+        public string ID
+        {
+            get
+            {
+                return this._ID;
+            }
+            set
+            {
+                this.OnPropertyChanged("ID", this._ID, value);
+                this._ID = value;
+            }
+        }
+
+        /// <summary>
+        ///  新闻ID,
+        /// </summary>
+
+        [DbProperty(MapingColumnName = "ContentId", DbTypeString = "char", ColumnIsNull = true, IsUnique = false, ColumnLength = 36, ColumnJingDu = 0, IsGenarator = false, StepSize = 0, ColumnDefaultValue = "")]
+
+        public string ContentId
+        {
+            get
+            {
+                return this._ContentId;
+            }
+            set
+            {
+                this.OnPropertyChanged("ContentId", this._ContentId, value);
+                this._ContentId = value;
+            }
+        }
+
+        /// <summary>
+        ///  评论内容,
+        /// </summary>
+
+        [DbProperty(MapingColumnName = "Description", DbTypeString = "nvarchar", ColumnIsNull = false, IsUnique = false, ColumnLength = 4000, ColumnJingDu = 0, IsGenarator = false, StepSize = 0, ColumnDefaultValue = "")]
+
+        public string Description
+        {
+            get
+            {
+                return this._Description;
+            }
+            set
+            {
+                this.OnPropertyChanged("Description", this._Description, value);
+                this._Description = value;
+            }
+        }
+
+        /// <summary>
+        ///  评论时间,
+        /// </summary>
+
+        [DbProperty(MapingColumnName = "CreatedDate", DbTypeString = "datetime", ColumnIsNull = false, IsUnique = false, ColumnLength = 0, ColumnJingDu = 0, IsGenarator = false, StepSize = 0, ColumnDefaultValue = "(getdate())")]
+
+        public DateTime CreatedDate
+        {
+            get
+            {
+                return this._CreatedDate;
+            }
+            set
+            {
+                this.OnPropertyChanged("CreatedDate", this._CreatedDate, value);
+                this._CreatedDate = value;
+            }
+        }
+
+        /// <summary>
+        ///  会员ID,
+        /// </summary>
+
+        [DbProperty(MapingColumnName = "CreatedUserID", DbTypeString = "char", ColumnIsNull = false, IsUnique = false, ColumnLength = 36, ColumnJingDu = 0, IsGenarator = false, StepSize = 0, ColumnDefaultValue = "")]
+
+        public string CreatedUserID
+        {
+            get
+            {
+                return this._CreatedUserID;
+            }
+            set
+            {
+                this.OnPropertyChanged("CreatedUserID", this._CreatedUserID, value);
+                this._CreatedUserID = value;
+            }
+        }
+
+        /// <summary>
+        ///  回复次数,
+        /// </summary>
+
+        [DbProperty(MapingColumnName = "ReplyCount", DbTypeString = "int", ColumnIsNull = false, IsUnique = false, ColumnLength = 0, ColumnJingDu = 0, IsGenarator = false, StepSize = 0, ColumnDefaultValue = "")]
+
+        public int ReplyCount
+        {
+            get
+            {
+                return this._ReplyCount;
+            }
+            set
+            {
+                this.OnPropertyChanged("ReplyCount", this._ReplyCount, value);
+                this._ReplyCount = value;
+            }
+        }
+
+        /// <summary>
+        ///  父评论ID,
+        /// </summary>
+
+        [DbProperty(MapingColumnName = "ParentID", DbTypeString = "char", ColumnIsNull = true, IsUnique = false, ColumnLength = 36, ColumnJingDu = 0, IsGenarator = false, StepSize = 0, ColumnDefaultValue = "")]
+
+        public string ParentID
+        {
+            get
+            {
+                return this._ParentID;
+            }
+            set
+            {
+                this.OnPropertyChanged("ParentID", this._ParentID, value);
+                this._ParentID = value;
+            }
+        }
+
+        /// <summary>
+        ///  类型ID,
+        /// </summary>
+
+        [DbProperty(MapingColumnName = "TypeID", DbTypeString = "char", ColumnIsNull = true, IsUnique = false, ColumnLength = 36, ColumnJingDu = 0, IsGenarator = false, StepSize = 0, ColumnDefaultValue = "")]
+
+        public string TypeID
+        {
+            get
+            {
+                return this._TypeID;
+            }
+            set
+            {
+                this.OnPropertyChanged("TypeID", this._TypeID, value);
+                this._TypeID = value;
+            }
+        }
+
+        /// <summary>
+        ///  状态,0未审核，1审核，2 审核不通过
+        /// </summary>
+
+        [DbProperty(MapingColumnName = "State", DbTypeString = "int", ColumnIsNull = false, IsUnique = false, ColumnLength = 0, ColumnJingDu = 0, IsGenarator = false, StepSize = 0, ColumnDefaultValue = "")]
+
+        public int State
+        {
+            get
+            {
+                return this._State;
+            }
+            set
+            {
+                this.OnPropertyChanged("State", this._State, value);
+                this._State = value;
+            }
+        }
+
+        /// <summary>
+        ///  已读,
+        /// </summary>
+
+        [DbProperty(MapingColumnName = "IsRead", DbTypeString = "bit", ColumnIsNull = false, IsUnique = false, ColumnLength = 0, ColumnJingDu = 0, IsGenarator = false, StepSize = 0, ColumnDefaultValue = "")]
+
+        public bool IsRead
+        {
+            get
+            {
+                return this._IsRead;
+            }
+            set
+            {
+                this.OnPropertyChanged("IsRead", this._IsRead, value);
+                this._IsRead = value;
+            }
+        }
+
+        /// <summary>
+        ///  评论人昵称,
+        /// </summary>
+
+        [DbProperty(MapingColumnName = "CreatedNickName", DbTypeString = "nvarchar", ColumnIsNull = true, IsUnique = false, ColumnLength = 400, ColumnJingDu = 0, IsGenarator = false, StepSize = 0, ColumnDefaultValue = "")]
+
+        public string CreatedNickName
+        {
+            get
+            {
+                return this._CreatedNickName;
+            }
+            set
+            {
+                this.OnPropertyChanged("CreatedNickName", this._CreatedNickName, value);
+                this._CreatedNickName = value;
+            }
+        }
+
+        #endregion
+
+        #region 列定义
+        public class Column
+        {
+            public Column(string tableName)
+            {
+
+                ID = new PropertyItem("ID", tableName);
+
+                ContentId = new PropertyItem("ContentId", tableName);
+
+                Description = new PropertyItem("Description", tableName);
+
+                CreatedDate = new PropertyItem("CreatedDate", tableName);
+
+                CreatedUserID = new PropertyItem("CreatedUserID", tableName);
+
+                ReplyCount = new PropertyItem("ReplyCount", tableName);
+
+                ParentID = new PropertyItem("ParentID", tableName);
+
+                TypeID = new PropertyItem("TypeID", tableName);
+
+                State = new PropertyItem("State", tableName);
+
+                IsRead = new PropertyItem("IsRead", tableName);
+
+                CreatedNickName = new PropertyItem("CreatedNickName", tableName);
+
+
+            }
+            /// <summary>
+            /// ID,
+            /// </summary> 
+            public PropertyItem ID = null;
+            /// <summary>
+            /// 新闻ID,
+            /// </summary> 
+            public PropertyItem ContentId = null;
+            /// <summary>
+            /// 评论内容,
+            /// </summary> 
+            public PropertyItem Description = null;
+            /// <summary>
+            /// 评论时间,
+            /// </summary> 
+            public PropertyItem CreatedDate = null;
+            /// <summary>
+            /// 会员ID,
+            /// </summary> 
+            public PropertyItem CreatedUserID = null;
+            /// <summary>
+            /// 回复次数,
+            /// </summary> 
+            public PropertyItem ReplyCount = null;
+            /// <summary>
+            /// 父评论ID,
+            /// </summary> 
+            public PropertyItem ParentID = null;
+            /// <summary>
+            /// 类型ID,
+            /// </summary> 
+            public PropertyItem TypeID = null;
+            /// <summary>
+            /// 状态,0未审核，1审核，2 审核不通过
+            /// </summary> 
+            public PropertyItem State = null;
+            /// <summary>
+            /// 已读,
+            /// </summary> 
+            public PropertyItem IsRead = null;
+            /// <summary>
+            /// 评论人昵称,
+            /// </summary> 
+            public PropertyItem CreatedNickName = null;
+        }
+        #endregion
+    }
+}
---
+++ src/EasyCms.Model/ShopProductReviews.cs
@@ -0,0 +1,396 @@
+﻿using Sharp.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+namespace EasyCms.Model
+{
+    /// <summary>
+    /// 商品评论
+    /// </summary>  
+    [JsonObject]
+    public partial class ShopProductReviews : BaseEntity
+    {
+        public static Column _ = new Column("ShopProductReviews");
+
+        public ShopProductReviews()
+        {
+            this.TableName = "ShopProductReviews";
+            OnCreate();
+        }
+
+
+        #region 私有变量
+
+        private string _ID;
+
+        private string _ProductId;
+
+        private string _UserId;
+
+        private string _ReviewText;
+
+        private string _UserName;
+
+        private string _UserEmail;
+
+        private DateTime _CreatedDate;
+
+        private string _ParentId;
+
+        private int _Status;
+
+        private string _OrderId;
+
+        private string _SKU;
+
+        private string _Attribute;
+
+        private string _ImagesID;
+
+        #endregion
+
+        #region 属性
+
+        /// <summary>
+        ///  主键ID,
+        /// </summary>
+
+        [PrimaryKey]
+        [DbProperty(MapingColumnName = "ID", DbTypeString = "char", ColumnIsNull = false, IsUnique = true, ColumnLength = 36, ColumnJingDu = 0, IsGenarator = true, StepSize = 1, ColumnDefaultValue = "")]
+
+        public string ID
+        {
+            get
+            {
+                return this._ID;
+            }
+            set
+            {
+                this.OnPropertyChanged("ID", this._ID, value);
+                this._ID = value;
+            }
+        }
+
+        /// <summary>
+        ///  商品ID,
+        /// </summary>
+
+        [DbProperty(MapingColumnName = "ProductId", DbTypeString = "char", ColumnIsNull = false, IsUnique = false, ColumnLength = 36, ColumnJingDu = 0, IsGenarator = false, StepSize = 0, ColumnDefaultValue = "")]
+
+        public string ProductId
+        {
+            get
+            {
+                return this._ProductId;
+            }
+            set
+            {
+                this.OnPropertyChanged("ProductId", this._ProductId, value);
+                this._ProductId = value;
+            }
+        }
+
+        /// <summary>
+        ///  会员ID,
+        /// </summary>
+
+        [DbProperty(MapingColumnName = "UserId", DbTypeString = "char", ColumnIsNull = false, IsUnique = false, ColumnLength = 36, ColumnJingDu = 0, IsGenarator = false, StepSize = 0, ColumnDefaultValue = "")]
+
+        public string UserId
+        {
+            get
+            {
+                return this._UserId;
+            }
+            set
+            {
+                this.OnPropertyChanged("UserId", this._UserId, value);
+                this._UserId = value;
+            }
+        }
+
+        /// <summary>
+        ///  评论内容,
+        /// </summary>
+
+        [DbProperty(MapingColumnName = "ReviewText", DbTypeString = "nvarchar", ColumnIsNull = false, IsUnique = false, ColumnLength = 4000, ColumnJingDu = 0, IsGenarator = false, StepSize = 0, ColumnDefaultValue = "")]
+
+        public string ReviewText
+        {
+            get
+            {
+                return this._ReviewText;
+            }
+            set
+            {
+                this.OnPropertyChanged("ReviewText", this._ReviewText, value);
+                this._ReviewText = value;
+            }
+        }
+
+        /// <summary>
+        ///  会员姓名,
+        /// </summary>
+
+        [DbProperty(MapingColumnName = "UserName", DbTypeString = "nvarchar", ColumnIsNull = true, IsUnique = false, ColumnLength = 400, ColumnJingDu = 0, IsGenarator = false, StepSize = 0, ColumnDefaultValue = "")]
+
+        public string UserName
+        {
+            get
+            {
+                return this._UserName;
+            }
+            set
+            {
+                this.OnPropertyChanged("UserName", this._UserName, value);
+                this._UserName = value;
+            }
+        }
+
+        /// <summary>
+        ///  会员邮箱,
+        /// </summary>
+
+        [DbProperty(MapingColumnName = "UserEmail", DbTypeString = "nvarchar", ColumnIsNull = true, IsUnique = false, ColumnLength = 100, ColumnJingDu = 0, IsGenarator = false, StepSize = 0, ColumnDefaultValue = "")]
+
+        public string UserEmail
+        {
+            get
+            {
+                return this._UserEmail;
+            }
+            set
+            {
+                this.OnPropertyChanged("UserEmail", this._UserEmail, value);
+                this._UserEmail = value;
+            }
+        }
+
+        /// <summary>
+        ///  评论日期,
+        /// </summary>
+
+        [DbProperty(MapingColumnName = "CreatedDate", DbTypeString = "datetime", ColumnIsNull = false, IsUnique = false, ColumnLength = 0, ColumnJingDu = 0, IsGenarator = false, StepSize = 0, ColumnDefaultValue = "")]
+
+        public DateTime CreatedDate
+        {
+            get
+            {
+                return this._CreatedDate;
+            }
+            set
+            {
+                this.OnPropertyChanged("CreatedDate", this._CreatedDate, value);
+                this._CreatedDate = value;
+            }
+        }
+
+        /// <summary>
+        ///  父ID,
+        /// </summary>
+
+        [DbProperty(MapingColumnName = "ParentId", DbTypeString = "char", ColumnIsNull = true, IsUnique = false, ColumnLength = 36, ColumnJingDu = 0, IsGenarator = false, StepSize = 0, ColumnDefaultValue = "")]
+
+        public string ParentId
+        {
+            get
+            {
+                return this._ParentId;
+            }
+            set
+            {
+                this.OnPropertyChanged("ParentId", this._ParentId, value);
+                this._ParentId = value;
+            }
+        }
+
+        /// <summary>
+        ///  状态,0未审核，1已审核，2审核不通过
+        /// </summary>
+
+        [DbProperty(MapingColumnName = "Status", DbTypeString = "int", ColumnIsNull = false, IsUnique = false, ColumnLength = 0, ColumnJingDu = 0, IsGenarator = false, StepSize = 0, ColumnDefaultValue = "")]
+
+        public int Status
+        {
+            get
+            {
+                return this._Status;
+            }
+            set
+            {
+                this.OnPropertyChanged("Status", this._Status, value);
+                this._Status = value;
+            }
+        }
+
+        /// <summary>
+        ///  订单ID,
+        /// </summary>
+
+        [DbProperty(MapingColumnName = "OrderId", DbTypeString = "char", ColumnIsNull = false, IsUnique = false, ColumnLength = 36, ColumnJingDu = 0, IsGenarator = false, StepSize = 0, ColumnDefaultValue = "((0))")]
+
+        public string OrderId
+        {
+            get
+            {
+                return this._OrderId;
+            }
+            set
+            {
+                this.OnPropertyChanged("OrderId", this._OrderId, value);
+                this._OrderId = value;
+            }
+        }
+
+        /// <summary>
+        ///  SKU码,
+        /// </summary>
+
+        [DbProperty(MapingColumnName = "SKU", DbTypeString = "nvarchar", ColumnIsNull = true, IsUnique = false, ColumnLength = 400, ColumnJingDu = 0, IsGenarator = false, StepSize = 0, ColumnDefaultValue = "")]
+
+        public string SKU
+        {
+            get
+            {
+                return this._SKU;
+            }
+            set
+            {
+                this.OnPropertyChanged("SKU", this._SKU, value);
+                this._SKU = value;
+            }
+        }
+
+        /// <summary>
+        ///  商品规格值,
+        /// </summary>
+
+        [DbProperty(MapingColumnName = "Attribute", DbTypeString = "text", ColumnIsNull = true, IsUnique = false, ColumnLength = 0, ColumnJingDu = 0, IsGenarator = false, StepSize = 0, ColumnDefaultValue = "")]
+
+        public string Attribute
+        {
+            get
+            {
+                return this._Attribute;
+            }
+            set
+            {
+                this.OnPropertyChanged("Attribute", this._Attribute, value);
+                this._Attribute = value;
+            }
+        }
+
+        /// <summary>
+        ///  晒图图片,
+        /// </summary>
+
+        [DbProperty(MapingColumnName = "ImagesID", DbTypeString = "char", ColumnIsNull = true, IsUnique = false, ColumnLength = 36, ColumnJingDu = 0, IsGenarator = false, StepSize = 0, ColumnDefaultValue = "")]
+
+        public string ImagesID
+        {
+            get
+            {
+                return this._ImagesID;
+            }
+            set
+            {
+                this.OnPropertyChanged("ImagesID", this._ImagesID, value);
+                this._ImagesID = value;
+            }
+        }
+
+        #endregion
+
+        #region 列定义
+        public class Column
+        {
+            public Column(string tableName)
+            {
+
+                ID = new PropertyItem("ID", tableName);
+
+                ProductId = new PropertyItem("ProductId", tableName);
+
+                UserId = new PropertyItem("UserId", tableName);
+
+                ReviewText = new PropertyItem("ReviewText", tableName);
+
+                UserName = new PropertyItem("UserName", tableName);
+
+                UserEmail = new PropertyItem("UserEmail", tableName);
+
+                CreatedDate = new PropertyItem("CreatedDate", tableName);
+
+                ParentId = new PropertyItem("ParentId", tableName);
+
+                Status = new PropertyItem("Status", tableName);
+
+                OrderId = new PropertyItem("OrderId", tableName);
+
+                SKU = new PropertyItem("SKU", tableName);
+
+                Attribute = new PropertyItem("Attribute", tableName);
+
+                ImagesID = new PropertyItem("ImagesID", tableName);
+
+
+            }
+            /// <summary>
+            /// 主键ID,
+            /// </summary> 
+            public PropertyItem ID = null;
+            /// <summary>
+            /// 商品ID,
+            /// </summary> 
+            public PropertyItem ProductId = null;
+            /// <summary>
+            /// 会员ID,
+            /// </summary> 
+            public PropertyItem UserId = null;
+            /// <summary>
+            /// 评论内容,
+            /// </summary> 
+            public PropertyItem ReviewText = null;
+            /// <summary>
+            /// 会员姓名,
+            /// </summary> 
+            public PropertyItem UserName = null;
+            /// <summary>
+            /// 会员邮箱,
+            /// </summary> 
+            public PropertyItem UserEmail = null;
+            /// <summary>
+            /// 评论日期,
+            /// </summary> 
+            public PropertyItem CreatedDate = null;
+            /// <summary>
+            /// 父ID,
+            /// </summary> 
+            public PropertyItem ParentId = null;
+            /// <summary>
+            /// 状态,0未审核，1已审核，2审核不通过
+            /// </summary> 
+            public PropertyItem Status = null;
+            /// <summary>
+            /// 订单ID,
+            /// </summary> 
+            public PropertyItem OrderId = null;
+            /// <summary>
+            /// SKU码,
+            /// </summary> 
+            public PropertyItem SKU = null;
+            /// <summary>
+            /// 商品规格值,
+            /// </summary> 
+            public PropertyItem Attribute = null;
+            /// <summary>
+            /// 晒图图片,
+            /// </summary> 
+            public PropertyItem ImagesID = null;
+        }
+        #endregion
+    }
+}
