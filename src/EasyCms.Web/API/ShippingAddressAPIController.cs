44d42927ad635d7cfee2ff727e0ee7a238c75629
luyonglie <luyonglie@163.com>
2015-07-26 00:07:59 +0800

    阿萨德
    撒的

--- src/EasyCms.Web/API/ShippingAddressAPIController.cs
+++ src/EasyCms.Web/API/ShippingAddressAPIController.cs
@@ -1,6 +1,7 @@
 ﻿using EasyCms.Business;
 using EasyCms.Model;
 using EasyCms.Web.Common;
+using Sharp.Common;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -26,7 +27,7 @@ namespace EasyCms.Web.API
                 if (string.IsNullOrWhiteSpace(userid))
                 {
                     return "无效令牌".FormatError();
-                    
+
                 }
                 else
                 {
@@ -88,41 +89,54 @@ namespace EasyCms.Web.API
             }
         }
 
-        public HttpResponseMessage Post([FromBody]ShopShippingAddress shopShippingAddress)
+        public HttpResponseMessage Post([FromBody]  ShopShippingAddress shopShippingAddress)
         {
             try
             {
                 //检验验证码是否正确，
                 string msg = string.Empty;
-                if (string.IsNullOrWhiteSpace(shopShippingAddress.UserId))
+                
+                if (shopShippingAddress.RegionId < 1)
                 {
-                    msg = "用户ID不能为空";
+                    msg = "收件地区不能为空";
                 }
                 else
-                    if (shopShippingAddress.RegionId < 1)
+                    if (string.IsNullOrWhiteSpace(shopShippingAddress.ShipName))
                     {
-                        msg = "收件地区不能为空";
+                        msg = "收件人姓名不能为空";
                     }
                     else
-                        if (string.IsNullOrWhiteSpace(shopShippingAddress.ShipName))
-                        {
-                            msg = "收件人姓名不能为空";
-                        }
 
-                if (string.IsNullOrWhiteSpace(shopShippingAddress.CelPhone))
-                {
-                    msg = "收件人电话不能为空";
-                }
-                else
-                    if (string.IsNullOrWhiteSpace(shopShippingAddress.Address))
-                    {
-                        msg = "详细地址不能为空";
-                    }
-                    else
-                    {
-                        shopShippingAddress.UserId = Request.GetAccountID();
-                        msg = new ShopShippingAddressBll().Save(shopShippingAddress);
-                    }
+                        if (string.IsNullOrWhiteSpace(shopShippingAddress.CelPhone))
+                        {
+                            msg = "收件人电话不能为空";
+                        }
+                        else
+                            if (string.IsNullOrWhiteSpace(shopShippingAddress.Address))
+                            {
+                                msg = "详细地址不能为空";
+                            }
+                            else
+                            {
+                                shopShippingAddress.UserId = Request.GetAccountID();
+                                ShopShippingAddressBll bll = new ShopShippingAddressBll();
+                                ShopShippingAddress entity = null;
+
+                                if (shopShippingAddress.RecordStatus == Sharp.Common.StatusType.update)
+                                {
+
+                                    entity = bll.GetEntity(shopShippingAddress.ID);
+                                    BaseEntity.CopyFrom<ShopShippingAddress>(entity, shopShippingAddress);
+
+                                }
+                                else
+                                {
+                                    
+                                    entity = shopShippingAddress;
+                                }
+                                
+                                msg = bll.Save(entity);
+                            }
 
                 bool isSuccess = msg == "添加成功";
                 if (isSuccess)
@@ -140,7 +154,7 @@ namespace EasyCms.Web.API
                 return ex.Format();
 
             }
-            
+
 
         }
 
