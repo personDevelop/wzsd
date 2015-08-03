55a10641f91e800e067b5e8afcbe5e3b94297b9c
luyonglie <luyonglie@163.com>
2015-08-03 10:30:49 +0800

    撒的
    阿萨德

--- src/EasyCms.Web/App_Start/RouteConfig.cs
+++ src/EasyCms.Web/App_Start/RouteConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
 
@@ -12,13 +13,18 @@ namespace EasyCms.Web
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-          
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
-             
+            routes.MapRoute(
+            name: "ApiWithWeb",
+            url: "api/{controller}/{action}/{id}/{pageIndex}/{other}",
+          defaults: new { id = RouteParameter.Optional, pageIndex = RouteParameter.Optional, other = RouteParameter.Optional });
+
+
         }
     }
 }
