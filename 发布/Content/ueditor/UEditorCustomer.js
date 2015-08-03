382caf5a7344c8c0cb51961dbd78cfbd39fec098
luyonglie <luyonglie@163.com>
2015-08-03 08:58:33 +0800

    阿萨德
    阿士大夫

---
+++ 发布/Content/ueditor/UEditorCustomer.js
@@ -0,0 +1,18 @@
+﻿//function getContent(editid) {
+//    //Html编码获取Html转义实体
+//    $('<div/>').text(UE.getEditor(editid).getContent()).html();
+//}
+
+//function setContent(editid, content, isAppendTo) {
+//    if (!isAppendTo) {
+//        isAppendTo = true;
+//    }
+//    //Html解码获取Html实体
+//    UE.getEditor(editid).setContent($('<div/>').html(content).text(), isAppendTo);
+//}
+//function getContentTxt(editid) {
+//    UE.getEditor(editid).getContentTxt();
+//}
+//function htmlDecode(value) {
+//    return;
+//}
